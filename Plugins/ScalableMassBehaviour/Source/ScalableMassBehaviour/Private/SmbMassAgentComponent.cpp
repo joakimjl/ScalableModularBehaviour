a30d638a5487ef07dd9fd950bd50558b4cb3a930
Joakim Svensson <48772061+joakimjl@users.noreply.github.com>
2025-11-14 09:17:08 +0000

    AddedAgentComponent

---
+++ Plugins/ScalableMassBehaviour/Source/ScalableMassBehaviour/Private/SmbMassAgentComponent.cpp
@@ -0,0 +1,49 @@
+﻿// Copyright 2025 Land Chaunax
+
+
+#include "SmbMassAgentComponent.h"
+
+#include "SmbFragments.h"
+
+
+// Sets default values for this component's properties
+USmbMassAgentComponent::USmbMassAgentComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void USmbMassAgentComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	EntityManager = UE::Mass::Utils::GetEntityManager(GetWorld());
+	// ...
+	
+}
+
+
+// Called every frame
+void USmbMassAgentComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                           FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+float USmbMassAgentComponent::GetHealth() const
+{
+	if (!EntityManager) return -2.f;
+	if (FDefenceFragment* DefenceFrag = EntityManager->GetFragmentDataPtr<FDefenceFragment>(AgentHandle))
+	{
+		return DefenceFrag->HP;
+	}
+	return -1.f;
+}
+
---
+++ Plugins/ScalableMassBehaviour/Source/ScalableMassBehaviour/Public/SmbMassAgentComponent.h
@@ -0,0 +1,32 @@
+﻿// Copyright 2025 Land Chaunax
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MassAgentComponent.h"
+#include "SmbMassAgentComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SCALABLEMASSBEHAVIOUR_API USmbMassAgentComponent : public UMassAgentComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	USmbMassAgentComponent();
+	
+	UFUNCTION(BlueprintCallable, Category = "Smb")
+	float GetHealth() const;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	FMassEntityManager* EntityManager = nullptr;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
